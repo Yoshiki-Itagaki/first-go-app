c7ce1ccbf415e301a56eeb14a3d3c8622f600e93
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-10-06 10:00:02 +0900

    Played around with variables with Go

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World🫡")
+
+	a := 42
+	fmt.Println(a)
+
+	b, c, d, _, f := 0, 1, 2, 3, "happiness"
+	fmt.Println(b, c, d, f)
+
+	var g int
+	fmt.Println(g)
+	g = 43
+	fmt.Println(g)
+
+	var h int = 44
+	fmt.Println(h)
+
+}
